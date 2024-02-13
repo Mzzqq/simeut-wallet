5572caaa9d45e6b7b24a5b88d29d35a9f8676c71
Mzzqq <rezarifa661@gmail.com>
2024-02-13 20:00:22 +0700

    refactor: Rearrange import statements

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,9 +1,8 @@
 use std::time::Duration;
+use eyre::{ContextCompat, Result};
+use hex::ToHex;
 
 use ethers::{
     prelude::{Address, LocalWallet, Middleware, Provider, Signer, TransactionRequest, U256},
     utils::Ganache,
 };
-
-use eyre::{ContextCompat, Result};
-use hex::ToHex;
